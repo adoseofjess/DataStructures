e2d95a8be3eff39365a5d228ba5059de7a5387c9
Jessica <jessjkim@gmail.com>
2016-04-26 14:37:19 -0400

    add stack

---
+++ Stack.rb
@@ -0,0 +1,21 @@
+class Stack
+	def initialize
+    @stack = []
+  end
+
+  def push(data)
+    @stack.push(data)
+  end
+
+  def pop
+    @stack.delete(@stack.last)
+  end
+
+  def empty?
+    @stack.empty?
+  end
+
+  def top
+    @stack.last
+  end
+end
